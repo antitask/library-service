75df9a3d294a2749ad0c184e604cf080e69411f6
Nikola Gavranovic <nikolagavranovic2021@gmail.com>
2023-05-25 17:57:00 +0200

    BookEntity, BookRepository

--- src/main/java/com/antitask/library/service/LibrarySeedService.java
+++ src/main/java/com/antitask/library/service/LibrarySeedService.java
@@ -25,21 +25,21 @@ public class LibrarySeedService {
     public void seed() {
         BookEntity book1 = new BookEntity(null, "The Hunger Games", "Marcos Quilasim", "8432-3532-7543-1234",
                 true, LocalDate.of(2021, 2, 24), "Johnny99");
-        BookEntity book2 = new BookEntity(null, "To Kill a Mockingbird", "Harper Lee", "9780061120084", true, LocalDate.of(2021, 3, 15), "Alice123");
+        BookEntity book2 = new BookEntity(null, "To Kill a Mockingbird", "Harper Lee", "9780061120084", true, LocalDate.of(2021, 3, 15), null);
 
-        BookEntity book3 = new BookEntity(null, "1984", "George Orwell", "9780451524935", true, LocalDate.of(2021, 4, 5), "Bob456");
+        BookEntity book3 = new BookEntity(null, "1984", "George Orwell", "9780451524935", true, LocalDate.of(2021, 4, 5), null);
 
-        BookEntity book4 = new BookEntity(null, "The Great Gatsby", "F. Scott Fitzgerald", "9780743273565", true, LocalDate.of(2021, 5, 1), "Carol789");
+        BookEntity book4 = new BookEntity(null, "The Great Gatsby", "F. Scott Fitzgerald", "9780743273565", true, LocalDate.of(2021, 5, 1), null);
 
         BookEntity book5 = new BookEntity(null, "Moby Dick", "Herman Melville", "9780142437247", false, null, null);
 
-        BookEntity book6 = new BookEntity(null, "War and Peace", "Leo Tolstoy", "9780140447934", true, LocalDate.of(2021, 6, 20), "David321");
+        BookEntity book6 = new BookEntity(null, "War and Peace", "Leo Tolstoy", "9780140447934", true, LocalDate.of(2021, 6, 20), null);
 
         BookEntity book7 = new BookEntity(null, "The Catcher in the Rye", "J.D. Salinger", "9780316769488", false, null, null);
 
-        BookEntity book8 = new BookEntity(null, "Pride and Prejudice", "Jane Austen", "9780141439518", true, LocalDate.of(2021, 7, 10), "Emily654");
+        BookEntity book8 = new BookEntity(null, "Pride and Prejudice", "Jane Austen", "9780141439518", true, LocalDate.of(2021, 7, 10), null);
 
-        BookEntity book9 = new BookEntity(null, "The Lord of the Rings", "J.R.R. Tolkien", "9780618640157", true, LocalDate.of(2021, 8, 5), "Frank789");
+        BookEntity book9 = new BookEntity(null, "The Lord of the Rings", "J.R.R. Tolkien", "9780618640157", true, LocalDate.of(2021, 8, 5), null);
 
         BookEntity book10 = new BookEntity(null, "The Chronicles of Narnia", "C.S. Lewis", "9780064471190", false, null, null);
 
