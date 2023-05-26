7b7b52392ab364e7b2a211b9e25269eb54dff571
antitask <admin@antitask.com>
2023-05-26 19:08:19 +0200

    allow CORS

---
+++ src/main/java/com/antitask/library/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.antitask.library.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        String[] allowDomains = new String[2];
+        allowDomains[0] = "http://localhost:4200";
+        allowDomains[1] = "http://localhost:8080";
+
+        System.out.println("CORS configuration....");
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins(allowDomains);
+            }
+        };
+    }
+}
